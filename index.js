63d9f1474d69c167a72956bd66e4c57fb6c47102
Mifaresss <9803211@gmail.com>
2023-03-17 16:53:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const TelegramApi = require('node-telegram-bot-api')
+const { Configuration, OpenAIApi } = require('openai')
+require('dotenv').config()
+
+const TOKEN_TELEGRAM = process.env.TOKEN_TELEGRAM
+const API_KEY_OPENAI = process.env.API_KEY_OPENAI
+
+const bot = new TelegramApi(TOKEN_TELEGRAM, { polling: true })
+const configuration = new Configuration({
+   apiKey: API_KEY_OPENAI,
+})
+const openai = new OpenAIApi(configuration)
+
+bot.on('message', async msg => {
+   const chatId = msg.chat.id
+   const message = msg.text
+
+   try {
+      const completion = await openai.createChatCompletion({
+         model: 'gpt-3.5-turbo',
+         messages: [{ role: 'user', content: message }],
+      })
+      bot.sendMessage(chatId, completion.data.choices[0].message.content.trim())
+   }
+   
+   catch (err) {
+      bot.sendMessage(chatId, `Ошибка( sorry`)
+   }
+})
